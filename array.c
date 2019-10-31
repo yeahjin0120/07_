9435623681c1c9ad9ae2ff719bbfbe2b99f0c900
yeahjin0120 <55428346+yeahjin0120@users.noreply.github.com>
2019-10-31 16:16:44 +0900

    06_matrix

--- array.c
+++ array.c
@@ -2,19 +2,35 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	int i;
-	int a[5] = {1, 2, 3, 4, 5};
-	int b[5] = {1, 2, 3, 4, 5};
-	
-	for (i=0; i<5; i++)
+void addMatrix(int A[][3], int B[][3], int C[][3])
 	{
-		if(a[i] != b[i])
-		{
-			printf ("%i is different\n", i);
-		}
+	int i ,j ;
+	for (i=0; i<3; i++)
+		for (j=0;j<3; j++)
+			C[i][j] = A[i][j] + B[i][j];
 	}
+void printMatrix (int A[][3])
+	{
+		int i,j;
+		for (i=0; i<3; i++)
+		{
+			for (j=0;j<3; j++)
+				printf("%i ", A[i][j]);
+		printf("\n");		
+		}
+	}	
+int main(int argc, char *argv[]) {
+	int A[3][3] = { //더하기 대상 행렬 
+	{2,3,0},
+	{8,9,1},
+	{7,0,5}};
+	int B[3][3] = { //더하기 대상 행렬 
+	{1,0,0},
+	{0,1,0},
+	{0,0,1}};
+	int C[3][3]; // 덧셈 결과 행렬 
 	
+	addMatrix (A,B,C);
+	printMatrix(C);
 	return 0;
 }
