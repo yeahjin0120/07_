b9fdf62ae4fe4025f18217788de127c2a745f686
yeahjin0120 <55428346+yeahjin0120@users.noreply.github.com>
2019-10-31 15:30:52 +0900

    03

--- main.c
+++ main.c
@@ -5,16 +5,22 @@
 
 int main(int argc, char *argv[]) {
 	int i;
-	int grade [5];
+	int grade[5];
+	int score[5];
 	
-	grade[0] = 10;
-	grade[1] = 20;
-	grade[2] = 30;
-	grade[3] = 40;
-	grade[4] = 50;
+	for (i=0; i<5; i++)
+	{
+		grade[i] = rand()%100;
+	}
+	for (i=0; i<5; i++)
+	{
+		score [i] = grade [i];
+	}
+	for (i=0; i<5; i++)
+	{
+		printf("score[%d]= %d\n", i , score[i]);
+	}
 	
-	for(i=0; i<5; i++)
-	printf("grade[%i] = %i\n", i,grade[i]);
 	
 	return 0;
 }
